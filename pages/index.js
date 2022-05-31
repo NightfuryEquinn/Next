7c86ca1b9411d3dccc911e70283193a3162c4b6a
Equinn Nightfury <xianzyip8@gmail.com>
2022-05-31 11:40:37 +0800

    Done next js header

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebDevz Newz</title>
+        <meta ame='keywords' content='web development, programming, next, js' />
+      </Head>
+
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
+
+export default Home
